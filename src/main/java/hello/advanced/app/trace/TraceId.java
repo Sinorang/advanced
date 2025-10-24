4c61741f85bbae6888760bb84edabd8a1552f3a4
sinorang <sinholang@naver.com>
2025-10-24 14:24:38 +0900

    feat: 로그 추적기 기본 기능 구현

--- src/main/java/hello/advanced/app/trace/TraceId.java
+++ src/main/java/hello/advanced/app/trace/TraceId.java
@@ -7,7 +7,7 @@ public class TraceId {
     private String id;
     private int level;
 
-    public TraceId(String id) {
+    public TraceId() {
         this.id = createdId();
         this.level = 0;
     }
@@ -22,7 +22,23 @@ public class TraceId {
         return UUID.randomUUID().toString().substring(0, 8);
     }
 
-    private TraceId createNextId() {
+    public TraceId createNextId() {
         return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
     }
 }
